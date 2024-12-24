96f95f3d69dbe22fa87156ae92ed8e519eb891a6
Aureliano Sinatra <aureliano.buendias@outlook.com>
2024-12-24 09:53:14 +0100

    test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cloud-computing
+
+```
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+pyenv install 3.10.4
+pyenv virtualenv 3.10.4 cloudcomputing
+```
---
+++ main.py
@@ -0,0 +1,27 @@
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+import smtplib, ssl
+
+# smtp = smtplib.SMTP('ssl0.ovh.net',587)
+smtp = smtplib.SMTP('ssl0.ovh.net',465)
+smtp.set_debuglevel(1)
+smtp.ehlo()
+# smtp.starttls()
+smtp.login("ynovcampus", "ynovcampus")
+
+subject = 'Bonsoir'
+text = "Bonjour"
+msg = MIMEMultipart()
+my_address = "ynovcampus@claveblanca.com"
+msg["From"] = my_address
+msg["To"] = "sinaure@gmail.com"
+msg['Subject'] = subject
+msg.attach(MIMEText(text))
+
+to = ["sinaure@gmail.com"]
+smtp.sendmail(from_addr=my_address,
+              to_addrs=to,
+              msg=msg.as_string())
+smtp.quit()
