0a45914245bb76728f8030613a1caa5ab2ee3132
Aureliano Sinatra <aureliano.buendias@outlook.com>
2025-06-06 11:32:52 +0200

    test deploy

---
+++ workshops/apps/kraken/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import krakenex
+from pykrakenapi import KrakenAPI
+api = krakenex.API()
+k = KrakenAPI(api)
+import time
+import sys
+
+pairs = ["BTCEUR","NANOEUR","ADAEUR","LINKEUR","XXRPZEUR","DOTEUR"]
+
+
+def main():
+    for pair in pairs:
+            try:
+                ohlc, last = k.get_ohlc_data(pair)
+                print("********* "+pair)
+                print(str(ohlc))
+                time.sleep(1)
+            except:
+                print("Unexpected error while fetching data for {}: {}", pair, sys.exc_info()[0])
+
