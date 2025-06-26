4432306538b3fda013c1934a6c83534669970832
Aureliano Sinatra <aureliano.buendias@outlook.com>
2025-06-26 14:34:57 +0200

    changet template for pyenv

--- workshops/network/output.tf
+++ workshops/network/output.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = digitalocean_vpc.network_vpc.id
 }
+
+data "digitalocean_region" "sfo2" {
+  slug = "sfo2"
+}
+
+output "regions" {
+  value = data.digitalocean_region.sfo2
+}
