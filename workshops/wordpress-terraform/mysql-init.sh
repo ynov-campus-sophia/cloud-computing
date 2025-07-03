26962d9afa8b90acb31afff7c13c24c775f3bfa7
clemoune-tech <clement.bouffard@ynov.com>
2025-07-03 15:37:16 +0200

    Delete workshops/wordpress-terraform/mysql-init.sh

--- workshops/wordpress-terraform/mysql-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt update && apt install -y mysql-server
-sed -i 's/bind-address.*/bind-address = 10.1.1.2/' /etc/mysql/mysql.conf.d/mysqld.cnf
-systemctl restart mysql
-
-mysql -e "CREATE DATABASE wordpress;"
-mysql -e "CREATE USER 'wpuser'@'10.1.1.1' IDENTIFIED BY 'wppassword';"
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'10.1.1.1';"
-mysql -e "FLUSH PRIVILEGES;"
